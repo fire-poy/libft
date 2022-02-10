acb997c4203b21bcc66f75d48dfc60401c0441bb
Mauro Pons <mpons@student.42lausanne.ch>
2022-02-10 16:18:38 +0100

    corriged ft_lstnew

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: mpons <marvin@42lausanne.ch>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 16:15:32 by mpons             #+#    #+#             */
-/*   Updated: 2021/12/21 18:05:17 by mpons            ###   ########.fr       */
+/*   Updated: 2022/01/09 18:06:01 by mpons            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -16,7 +16,7 @@ t_list	*ft_lstnew(void *content)
 {
 	t_list	*block;
 
-	block = malloc(sizeof(block));
+	block = malloc(sizeof(t_list));
 	if (!block)
 		return (NULL);
 	block->content = content;
@@ -30,6 +30,10 @@ int	main(void)
 	t_list *debut;
 
 	debut = &elem1;
+	printf("size of t_list %lu\n", sizeof(t_list));
+	printf("size of elem1 %lu\n", sizeof(elem1));
+	printf("size of debut %lu\n", sizeof(debut));
+	printf("size of *debut %lu\n", sizeof(*debut));
 	elem1 = *ft_lstnew("hola manola");
 	printf("valeur du 1er element = %s\n", debut->content); 
 	return (0);
